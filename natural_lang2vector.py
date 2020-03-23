e20fbcb68c3474aa8c1206d41186e70366ce832e
david <sirchaos6@gmail.com>
2020-03-22 23:08:33 -0400

    debugging

--- natural_lang2vector.py
+++ natural_lang2vector.py
@@ -18,54 +18,60 @@ class BertEmbedd:
         # bert model
         self.bert = BertEmbedding(model="bert_12_768_12")
 
-    def fuse(self, word_emb):
-        new = np.empty(word_emb[1][0].shape)
-        counter = 0.0
-        for tok, vect in zip(word_emb[0], word_emb[1]):
-            if len(tok) != 0:
-                new += vect
-                counter += 1.0
-        return new/counter
+    def removeNanInfAndOvermax(self, an_array):
+        max_supported = np.finfo(np.float64).max
+        # remove NaN
+        if np.isnan(an_array.any()):
+            an_array = np.nan_to_num(an_array)
+        # remove inf & -inf
+        if np.isfinite(an_array.all()) is False:
+            an_array = np.array([f if f != float("inf") else max_supported for f in an_array])
+            an_array = np.array([f if f != float("-inf") else (-1 * max_supported) for f in an_array])
+        # remove overmax
+        an_array = np.array([f if f < max_supported else max_supported for f in an_array])
+        an_array = np.array([f if f > (-1 * max_supported) else (-1 * max_supported) for f in an_array])
+        return an_array
 
-    def fuse_all(self, word_emb_list):
-        for ind, w_emb in enumerate(word_emb_list):
-            word_emb_list[ind] = self.fuse(w_emb)
-        return word_emb_list
+    def fuse(self, word_emb):
+        new = np.average(word_emb[1], axis=0)
+        # reduce the size of float to the maximum supported by float64
+        new = self.removeNanInfAndOvermax(np.array(new))
+        return new
 
     def find_nearest(self, a_array, array_of_arrays, n=1):
         n = len(array_of_arrays) if n > len(array_of_arrays) else n
-        a_array = np.array([a_array])
-        a_array = np.nan_to_num(a_array)
-        print(777777777777, np.finfo(np.float64).max)
+        a_array = a_array.reshape(1, -1)
         # get 1 nearest neighbor using "ball tree" ('auto', 'ball_tree', 'kd_tree', 'brute')
         potential_neighbors = NearestNeighbors(n_neighbors=n, algorithm='ball_tree')
-        print(88888, np.isnan(array_of_arrays.any()))
-        print(99999, np.isfinite(array_of_arrays.all()))
         potential_neighbors = potential_neighbors.fit(array_of_arrays)
-        # potential_neighbors = potential_neighbors.fit_transform(potential_neighbors)
+        # potential_neighbors = potential_neighbors.fit_transform(array_of_arrays)
         distances, indices = potential_neighbors.kneighbors(a_array)
-        return indices[0]
+        indices = indices[0].tolist()
+        for i, (dist, ind) in enumerate(zip(distances[0], indices)):
+            indices[i] = ind if dist != float("inf") else None
+        return indices
 
 class WiktionaryData:
     """
     Modify the wiktionary extracted data (as json) to output a vector representation
     """
 
-    def __init__(self, in_file_path, out_file_path):
+    def __init__(self, in_file_path, out_file_path=None, in_ieml_file_path=None):
         # input file
-        self.json_in_path = in_file_path
-        self.json_file = open(in_file_path)
+        self.json_in_path = in_file_path if in_file_path is not None else in_ieml_file_path
+        self.json_file = open(self.json_in_path)
         # output file
-        with open(out_file_path, "w") as output_file:
-            output_file.write("")
-        self.out_file_path = out_file_path
-        self.out_file = open(out_file_path, "a")
-        # vocabulary file
-        vocab_path = "{0}.vocab".format(out_file_path.replace(".tsv", ""))
-        with open(vocab_path, "w") as output_vocab_file:
-            output_vocab_file.write("")
-        self.vocab_file_path = vocab_path
-        self.vocab_file = open(vocab_path, "a")
+        if out_file_path is not None:
+            with open(out_file_path, "w") as output_file:
+                output_file.write("")
+            self.out_file_path = out_file_path
+            self.out_file = open(out_file_path, "a")
+            # vocabulary file
+            vocab_path = "{0}.vocab".format(out_file_path.replace(".tsv", ""))
+            with open(vocab_path, "w") as output_vocab_file:
+                output_vocab_file.write("")
+            self.vocab_file_path = vocab_path
+            self.vocab_file = open(vocab_path, "a")
 
     def reset_json_in_file(self):
         self.json_file.close()
@@ -109,8 +115,8 @@ class WiktionaryData:
                 # next
                 ln = self.json_file.readline()
                 counter += 1 ###################
-                if counter == 2:
-                    break
+                # if counter == 50:
+                #     break
         self.reset_json_in_file()
         # open the sentence file and transform it to bert embeddings
         with open(sent_file_path) as sent_file:
@@ -139,9 +145,19 @@ class IemlData:
     Modify the ieml data to output a vector representation
     """
 
-    def __init__(self, database_folder_path):
+    def __init__(self, input_database_folder_path, out_file_path=None):
         from ieml.ieml_database import IEMLDatabase
-        self.database = IEMLDatabase(folder=database_folder_path)
+        # input file
+        self.database = IEMLDatabase(folder=input_database_folder_path)
+        # output file
+        if out_file_path is not None:
+            with open(out_file_path, "w") as output_file:
+                output_file.write("")
+            self.out_file_path = out_file_path
+            self.out_file = open(out_file_path, "a")
+
+    def close_all(self):
+        self.out_file.close()
 
     def get_word_objects(self):
         return self.database.list(parse=False, type='word')
@@ -171,7 +187,7 @@ class IemlData:
         bert_class = bert_class if bert_class is not None else BertEmbedd()
         return bert_class.bert([string])
 
-    def make_bert_emb_list(self, lang="en", bert_class=None):
+    def make_bert_emb_list(self, lang="en", bert_class=None, dump=False):
         bert_class = bert_class if bert_class is not None else BertEmbedd()
         bert_embeddings = []
         for ieml_pm_in_nl in self.get_natural_lang_meanings(lang):
@@ -181,7 +197,7 @@ class IemlData:
         return bert_embeddings
 
 
-def choose_task(t_task=None, input_path=None, output_path=None):
+def choose_task(t_task=None, input_path=None, output_path=None, input_ieml_path=None):
     """
     Choose and launch a task.
     :param t_task:
@@ -190,36 +206,52 @@ def choose_task(t_task=None, input_path=None, output_path=None):
     :return:
     """
     bert_class = BertEmbedd()
-    wiki_data = WiktionaryData(input_path, output_path)
-    ieml_data = IemlData("./ieml-language-master/")
-    if t_task.lower() in ["json2list", "list", "json", "makelist"]:
-        wiktionary_vector_data = wiki_data.make_bert_emb_list(bert_class, dump=True)
-        wiktionary_sents = [se[0][0] for se in wiktionary_vector_data if len(se[0][0]) != 0]
-        wiktionary_embeddings = np.array([bert_class.fuse(se) for se in wiktionary_vector_data])
-        # for w in wiktionary_embeddings:
-        #     print(w[0])
-        #######################
-        ieml_vector_data = ieml_data.make_bert_emb_list(lang="en", bert_class=bert_class)[0]
+    # COMMAND:
+    # python3 natural_lang2vector.py -t findnearest -in ./wiktionary-dumps/wiktionary_English.json -indb ./ieml-language-master/
+    if t_task.lower() in ["find", "findnear", "findnearest", "nearest"]:
+        wiki_data = WiktionaryData(input_path, output_path)
+        ieml_data = IemlData(input_ieml_path)
+        # make the embeddings from scratch
+        wiktionary_vector_data = wiki_data.make_bert_emb_list(bert_class)
+        # load the embeddings from file
+        # TODO FUNCT LOAD
+        ##############################
+        wiktionary_sents = [se[0] for se in wiktionary_vector_data if len(se[0]) != 0]
+        wiktionary_embeddings = np.array([bert_class.fuse(se) for se in wiktionary_vector_data if len(se[0][0]) != 0])
+        ieml_vector_data = ieml_data.make_bert_emb_list(lang="en", bert_class=bert_class)
         ieml_sents = [se[0][0] for se in ieml_vector_data if len(se[0][0]) != 0]
-        ieml_embeddings = np.array([bert_class.fuse(se) for se in ieml_vector_data])
-        for w in ieml_embeddings:
-            close_neighb_ind = bert_class.find_nearest(w, wiktionary_embeddings)
-            print(close_neighb_ind)
-        #######################
+        ieml_embeddings = np.array([bert_class.fuse(se[0]) for se in ieml_vector_data if len(se[0][0]) != 0])
+        for i, w in enumerate(ieml_embeddings):
+            close_neighb_ind = bert_class.find_nearest(w, wiktionary_embeddings, n=1)
+            print(111111, i, ieml_sents[i])
+            print(2222222, close_neighb_ind[0], wiktionary_sents[close_neighb_ind[0]])
+        ############################################
+    # COMMAND:
+    # python3 natural_lang2vector.py -t dumpwiktio -in ./wiktionary-dumps/wiktionary_English.json -out embeddings/wiktionary_English_embedd.tsv
+    elif t_task.lower() in ["json2list", "dumpwiktio", "json", "dumpw"]:
+        wiki_data = WiktionaryData(input_path, output_path, input_ieml_path)
+        wiktionary_vector_data = wiki_data.make_bert_emb_list(bert_class, dump=True)
+    # COMMAND:
+    # python3 natural_lang2vector.py -t dumpwiktio -in ./ieml-language-master/ -out embeddings/ieml_English_embedd.tsv
+    elif t_task.lower() in ["ieml2list", "dumpieml", "ieml", "dumpi"]:
+        ieml_data = IemlData(input_path, output_path, input_ieml_path)
+        ieml_vector_data = ieml_data.make_bert_emb_list(lang="en", bert_class=bert_class, dump=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--task", type=str, default="None", help="path to the wiktionary json")
-    parser.add_argument("-in", "--input", type=str, help="path to the wiktionary json")
+    parser.add_argument("-in", "--input", type=str, default="None", help="path to the wiktionary json")
+    parser.add_argument("-indb", "--inputDatabase", type=str, default="None", help="path to the ieml database folder")
     parser.add_argument("-out", "--output", type=str, help="path to the output file")
     args = parser.parse_args()
 
     a_task = args.task
     input_path = args.input
+    input_path = None if input_path in ["None", "none"]
+    input_ieml_path = args.inputDatabase
+    input_ieml_path = None if input_path in ["None", "none"]
     output_path = args.output
+    output_path = None if input_path in ["None", "none"]
 
-    choose_task(a_task, input_path, output_path)
-
-    # COMMAND
-    # python3 natural_lang2vector.py -t makelist -in ./wiktionary-dumps/wiktionary_English.json -out embeddings/wiktionary_English_embedd.tsv
+    choose_task(a_task, input_path, output_path, input_ieml_path)
